a0d23e6bc6d2acae9d7ea8bfddb44b0db5af9a83
Katerin Guerrero <katerinlorena1025@gmail.com>
2022-09-27 22:25:37 -0500

    Trabajos

---
+++ clases samuel/clase sep 26/desarrollo web.js
@@ -0,0 +1,12 @@
+//web sercicio que se puede encontrar 
+//ordenador es el cliente y va al servidor 
+//cliente servidor 
+//servidores estan preparados par varios equipos y sea masivo SEVIDOR DNS
+//DOMINAR el tema de red
+//ping comando para mostrar la red 
+//Network + url de la pagina
+//clases de direcciones ip 
+//NAT TRADUCE DIRECCIONES PRIVADAS A DICREECIONES PUNLICAS
+//ipconfig = informacion asociada a la red
+//dns trabaja de la mano con ips
+//protocolo mas importante http
